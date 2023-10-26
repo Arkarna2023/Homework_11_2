3ae7d941528680dcd6037296280d329775fd4192
Ekaterina Salnikova <sekateri@mail.ru>
2023-10-26 23:40:18 +0300

    change str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def get_capital_letters(value):
+    """возвращает заглавные буквы"""
     return value.upper()
 
+def get_capital_letter(value):
+    """функцию, которая делает заглавными первые буквы каждого слова в строке,
+    поступившей на вход функции."""
+    return value.title()
